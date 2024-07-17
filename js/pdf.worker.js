db80781737a078bb44ceac7b9d024e46124e93e7
Sanadi <79256438+sanadiibnuahmad@users.noreply.github.com>
2024-07-17 19:55:30 +0700

    Create pdf.worker.js

---
+++ js/pdf.worker.js
@@ -0,0 +1,8 @@
+/*! 
+ * pdf.worker.js v2.10.377
+ * https://mozilla.github.io/pdf.js/
+ * Copyright 2012 Mozilla Foundation
+ * Licensed under Apache License 2.0
+ */
+!function(e,t){"object"==typeof exports&&"undefined"!=typeof module?t(exports):"function"==typeof define&&define.amd?define(["exports"],t):t((e=e||self).pdfjsWorker={})}(this,(function(e){"use strict"; ... }));
+
