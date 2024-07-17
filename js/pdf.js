7eb19cbfa3ab677f5b576d3aa05122d2a1f78b89
Sanadi <79256438+sanadiibnuahmad@users.noreply.github.com>
2024-07-17 19:53:28 +0700

    Create pdf.js

---
+++ js/pdf.js
@@ -0,0 +1,8 @@
+/*! 
+ * pdf.js v2.10.377
+ * https://mozilla.github.io/pdf.js/
+ * Copyright 2012 Mozilla Foundation
+ * Licensed under Apache License 2.0
+ */
+!function(e,t){"object"==typeof exports&&"undefined"!=typeof module?t(exports):"function"==typeof define&&define.amd?define(["exports"],t):t((e=e||self).pdfjsLib={})}(this,(function(e){"use strict"; ... }));
+
